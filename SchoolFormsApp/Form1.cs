26973c5f64ab6e8475352f54b3e7f58f234fec22
DeveloperWizard83 <laaksomik@gmail.com>
2020-02-06 21:15:08 +0100

    Add project files.

---
+++ SchoolFormsApp/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonPushToTest_Click(object sender, EventArgs e)
+        {
+            var testSchool = new SchoolLibrary.School();
+            testSchool.Name = "Central City High";
+            testSchool.TwitterAddress = "CentralCity";
+        }
+    }
+}
